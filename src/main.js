836ed5c7434b5e38fe3a738e0a1d592a2ea38acf
NubScript7 <nubscript7.gmail.com>
2024-06-19 19:47:39 +0800

    trial and error v2 #4

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ if(process.argv.includes("--dotenv")){
   require("dotenv").config({path: path.resolve(__dirname,"../.env")})
 }
 const express = require("express");
+const axios = require("axios");
 const cors = require("cors");
 const ai = require("./gemini");
 const IncompleteEnvironmentVariableError = require("./IncompleteEnvironmentVariableError")
