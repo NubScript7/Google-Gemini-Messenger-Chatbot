e7fd3079120a4c7434bd2d58739ac3653fc89baf
NubScript7 <nubscript7.gmail.com>
2024-08-11 08:14:26 +0800

    i dunno what im doing (app v1)

--- legacy_commonjs/errors/GeminiAISpecifiedMaxSessionsReachedError.js
+++
@@ -1,9 +0,0 @@
-class GeminiAISpecifiedMaxSessionsReachedError extends Error {
-    constructor(message)
-    {
-        super(message);
-        this.name = this.constructor.name;
-    }
-}
-
-module.exports = GeminiAISpecifiedMaxSessionsReachedError;
--- legacy_commonjs/errors/GoogleGeminiGenerativeAISessionNotYetInitializedError.js
+++
@@ -1,9 +0,0 @@
-class GoogleGeminiGenerativeAISessionNotYetInitializedError extends Error {
-    constructor(message)
-    {
-        super(message);
-        this.name = this.constructor.name;
-    }
-}
-
-module.exports = GoogleGeminiGenerativeAISessionNotYetInitializedError;
